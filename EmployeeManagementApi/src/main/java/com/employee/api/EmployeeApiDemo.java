a9d6110a5103447e913459e93b0ae136e2301f6a
Akshay Malapure <akshaymalapure@gmail.com>
2020-06-29 01:00:15 +0530

    main class for Employee api demo

---
+++ EmployeeManagementApi/src/main/java/com/employee/api/EmployeeApiDemo.java
@@ -0,0 +1,19 @@
+
+package com.employee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author akshay
+ *Main class for Employee api
+ */
+@SpringBootApplication
+
+public class EmployeeApiDemo {
+
+	public static void main(String[] args) {
+  SpringApplication.run(EmployeeApiDemo.class, args); 
+  }
+  
+  }
