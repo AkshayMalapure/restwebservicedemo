cf1652ec96d9c6fa2c0b29e70adafb6045c47655
Akshay Malapure <akshaymalapure@gmail.com>
2020-06-29 01:24:00 +0530

    initial test file

---
+++ EmployeeManagementApi/src/test/java/com/employee/api/EmployeeDemoApiTests.java
@@ -0,0 +1,14 @@
+
+package com.employee.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDemoApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
